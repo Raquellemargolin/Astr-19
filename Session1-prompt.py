241b3b8c204667e94c3e48f366b4c743e719be42
Raquellemargolin <102757481+Raquellemargolin@users.noreply.github.com>
2022-04-22 16:45:52 -0700

    Add files via upload

---
+++ Session1-prompt.py
@@ -0,0 +1,8 @@
+def name_pronouns():
+	print("Raquelle Margolin, She/Her/Hers. My favorite movie is anything with Rachel Mcadams in it. My favorite food is a good, old fashioned cheeseburger.")
+
+def main():
+	name_pronouns()
+
+if __name__ == '__main__':
+	main()
